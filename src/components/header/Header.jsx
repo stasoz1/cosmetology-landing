7066064a5df819a229198296a1cf671b183f2f48
vladbrownDev <dev.vladbrown@gmai.com>
2021-09-26 20:31:35 +0300

    header-done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import './Header.css'
 
+import logo from "../../images/logo.png"
+
 const Header = () => {
     return (
-        <>
-        <div>Header</div>
-        <div>Test</div>
-        </>
+        <header>
+            <nav>
+                <img alt="logo" src={logo}/>
+                <div id="navbar">
+                    <div>Главная</div>
+                    <div>Обо мне</div>
+                    <div>Услуги</div>
+                    <div>Отзывы</div>
+                    <div>Контакты</div>
+                    <div>Связаться со мной</div>
+                </div>
+            </nav>
+            <div id="headerMain">
+                <h1>
+                    Сделай шаг к здоровой коже
+                </h1>
+                <div id="underHeader">
+                    Дерматокосметолог Елена Бастрикова
+                </div>
+                <button>
+                    Связаться
+                </button>
+            </div>  
+        </header>
     )
 }
 
