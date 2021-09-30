cee30d60ab17c8568f5d967c8b169257e765a981
vladbrownDev <dev.vladbrown@gmai.com>
2021-09-30 13:31:18 +0300

    photoComp-done

--- src/components/photos/Photos.jsx
+++ src/components/photos/Photos.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './Photos.css'
 
+import photo from "../../images/photoComp/1.png"
+
 const Photos = () => {
     return (
-        <div>Photos</div>
+        <section id="photoSec">
+            <div className="sectionLeft" >
+                <h1 className="mainHeading">
+                    Фото
+                </h1>
+                <div className="sectionDesc" id="photosDesc">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </div>
+            </div>
+            <div id="photoList">
+                <img src={photo} alt="examplePhoto" />
+                <img src={photo} alt="examplePhoto" />
+                <img src={photo} alt="examplePhoto" />
+                <img src={photo} alt="examplePhoto" />
+                <img src={photo} alt="examplePhoto" />
+                <img src={photo} alt="examplePhoto" />
+                <img src={photo} alt="examplePhoto" />
+                <img src={photo} alt="examplePhoto" />
+                <img src={photo} alt="examplePhoto" />
+            </div>
+        </section>
+        
+        
     )
 }
 
